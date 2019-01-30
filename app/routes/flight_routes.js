6b926137f16ed656c7379500f0172f777bd7f6de
David A Pears <davidapears@gmail.com>
2019-01-30 16:37:41 +0000

    ADDS Delete (delete) and Update (put) routes, tested in Postman

--- app/routes/flight_routes.js
+++ app/routes/flight_routes.js
@@ -1,8 +1,9 @@
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
-  app.get('/flights/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
+
+app.get('/flights/:id', (req, res) => {
+  const id = req.params.id;
+  const details = { '_id': new ObjectID(id) };
     db.collection('flights').findOne(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
@@ -11,6 +12,44 @@ module.exports = function(app, db) {
       }
     });
   });
+
+app.delete('/flights/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('flights').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Flight ' + id + ' deleted!');
+      }
+    });
+  });
+
+app.put('/flights/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const flight = {
+      FlightNo:   req.body.FlightNo,
+      Date:       req.body.Date,
+      DTime:      req.body.Time,
+      ArrDep:     req.body.ArrDep,
+      PortOfCall: req.body.PortOfCallA,
+      Status:     req.body.Status,
+      Other:      req.body.OtherInfo,
+      Additional: req.body.Addittional,
+      Airline:    req.body.Airline,
+      Image:      req.body.Image,
+      Arrival:    req.body.ArrHall
+     };
+    db.collection('flights').update(details, flight, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(flight);
+      }
+    });
+  });
+
 app.post('/flights', (req, res) => {
   const flight = {
     FlightNo:   req.body.FlightNo,
