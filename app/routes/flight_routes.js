82e9651cac2e73e1fbc6700fa9f889eebf07ea78
David A Pears <davidapears@gmail.com>
2019-01-30 16:32:14 +0000

    ALLOWS the reading (GETTING) of any flight by ID, tested with Postman

--- app/routes/flight_routes.js
+++ app/routes/flight_routes.js
@@ -1,27 +1,30 @@
+var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
-  app.post('/flights', (req, res) => {
-    console.log(req.body)
-    res.send('Hello')
+  app.get('/flights/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('flights').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
   });
-};
-
-
-module.exports = function(app, db) {
-  const collection =
-  app.post('/flights', (req, res) => {
-    const flight = {
-      FlightNo:   req.body.FlightNo,
-      Date:       req.body.Date,
-      DTime:      req.body.Time,
-      ArrDep:     req.body.ArrDep,
-      PortOfCall: req.body.PortOfCallA,
-      Status:     req.body.Status,
-      Other:      req.body.OtherInfo,
-      Additional: req.body.Addittional,
-      Airline:    req.body.Airline,
-      Image:      req.body.Image,
-      Arrival:    req.body.ArrHall
-     };
+app.post('/flights', (req, res) => {
+  const flight = {
+    FlightNo:   req.body.FlightNo,
+    Date:       req.body.Date,
+    DTime:      req.body.Time,
+    ArrDep:     req.body.ArrDep,
+    PortOfCall: req.body.PortOfCallA,
+    Status:     req.body.Status,
+    Other:      req.body.OtherInfo,
+    Additional: req.body.Addittional,
+    Airline:    req.body.Airline,
+    Image:      req.body.Image,
+    Arrival:    req.body.ArrHall
+   };
     db.collection('flights').insert(flight, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
